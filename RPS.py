6d8062e3084aa494e2883c7daa8494d9c9add034
Marius <mariusmaaland@gmail.com>
2015-08-23 18:44:19 +0200

    Add KMP and start husk > 1

--- RPS.py
+++ RPS.py
@@ -1,6 +1,9 @@
+from __future__ import generators
+
 __author__ = 'Marius'
 
 from random import randint
+
 
 
 
@@ -162,6 +165,11 @@ class Historiker:
                         return "saks"
                 else:
                     return self.counter(target)
+            elif self.husk > 1:
+                if len(self.history) >= self.husk:
+                    return next(self.KnuthMorrisPratt(self.history, self.history[-self.husk:]))
+
+
         else:
             n = randint(0,2)
             if n == 0:
@@ -208,6 +216,42 @@ class Historiker:
 
 
 
+        # Knuth-Morris-Pratt string matching
+    # David Eppstein, UC Irvine, 1 Mar 2002
+
+
+
+    def KnuthMorrisPratt(self, text, pattern):
+
+        '''Yields all starting positions of copies of the pattern in the text.
+    Calling conventions are similar to string.find, but its arguments can be
+    lists or iterators, not just strings, it returns all matches, not just
+    the first one, and it does not need the whole text in memory at once.
+    Whenever it yields, it will have read the text exactly up to and including
+    the match that caused the yield.'''
+
+        # allow indexing into pattern and protect against change during yield
+        pattern = list(pattern)
+
+        # build table of shift amounts
+        shifts = [1] * (len(pattern) + 1)
+        shift = 1
+        for pos in range(len(pattern)):
+            while shift <= pos and pattern[pos] != pattern[pos-shift]:
+                shift += shifts[pos-shift]
+            shifts[pos+1] = shift
+
+        # do the actual search
+        startPos = 0
+        matchLen = 0
+        for c in text:
+            while matchLen == len(pattern) or \
+                  matchLen >= 0 and pattern[matchLen] != c:
+                startPos += shifts[matchLen]
+                matchLen -= shifts[matchLen]
+            matchLen += 1
+            if matchLen == len(pattern):
+                yield startPos
 
 
 
@@ -229,7 +273,10 @@ class Historiker:
 
 
 
-spiller1 = Historiker(1)
+
+
+
+spiller1 = Historiker(2)
 spiller2 = Tilfeldig()
 print (spiller1.oppgi_navn())
 print (spiller2.oppgi_navn())
@@ -237,39 +284,22 @@ print ("Tilfeldig spiller " + spiller2.velg_aksjon())
 print ("Tilfeldiig spilte " + spiller2.motta_resultat())
 spiller1.addAction(spiller2.motta_resultat())
 print (spiller1.history)
-print ("Historiker spiller " + spiller1.velg_aksjon())
-
+print (spiller1.velg_aksjon())
 print ("Tilfeldig spiller " + spiller2.velg_aksjon())
 print ("Tilfeldiig spilte " + spiller2.motta_resultat())
 spiller1.addAction(spiller2.motta_resultat())
 print (spiller1.history)
-print ("Historiker spiller " + spiller1.velg_aksjon())
-
+print (spiller1.velg_aksjon())
 print ("Tilfeldig spiller " + spiller2.velg_aksjon())
 print ("Tilfeldiig spilte " + spiller2.motta_resultat())
 spiller1.addAction(spiller2.motta_resultat())
 print (spiller1.history)
-print ("Historiker spiller " + spiller1.velg_aksjon())
-
+print (spiller1.velg_aksjon())
 print ("Tilfeldig spiller " + spiller2.velg_aksjon())
 print ("Tilfeldiig spilte " + spiller2.motta_resultat())
 spiller1.addAction(spiller2.motta_resultat())
 print (spiller1.history)
-print ("Historiker spiller " + spiller1.velg_aksjon())
-
-print ("Tilfeldig spiller " + spiller2.velg_aksjon())
-print ("Tilfeldiig spilte " + spiller2.motta_resultat())
-spiller1.addAction(spiller2.motta_resultat())
-print (spiller1.history)
-print ("Historiker spiller " + spiller1.velg_aksjon())
-
-print ("Tilfeldig spiller " + spiller2.velg_aksjon())
-print ("Tilfeldiig spilte " + spiller2.motta_resultat())
-spiller1.addAction(spiller2.motta_resultat())
-print (spiller1.history)
-print ("Historiker spiller " + spiller1.velg_aksjon())
-print ("tilfeldig spiller " + spiller2.velg_aksjon())
-print (spiller1.motta_resultat())
+print (spiller1.velg_aksjon())
 
 
 
