c45a1586bf9ba8ee7a6f3fbc86904793a238bfa4
maaland <mariusmaaland@gmail.com>
2015-08-25 12:49:41 +0200

    Add code for GUI

--- RPS.py
+++ RPS.py
@@ -4,6 +4,16 @@ __author__ = 'Marius'
 
 from random import randint
 
+# Import statements
+# Disse brukes for å hente inn pakker til programmet vårt.
+# Beskrivelse på wiki om hvordan disse installeres på maskinen og gjøres tilgjengelig
+from tkinter import Tk, BOTH, StringVar
+from tkinter.ttk import Frame, Button, Label, Style
+import pylab
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
+
 
 
 
@@ -374,6 +384,7 @@ class EnkeltSpill:
 
 
 
+
 '''spiller1 = Historiker(2)
 spiller2 = Tilfeldig()
 print (spiller1.oppgi_navn())
@@ -425,15 +436,120 @@ print (action1.__eq__(action2))
 print (action1.__gt__(action2))
 print (action3.__gt__(action1))
 print (action3.__eq__(action1))'''
-spill = EnkeltSpill("historiker", "sekvensiell")
+'''spill = EnkeltSpill("historiker", "sekvensiell")
 print (spill.spiller1.oppgi_navn())
 print (spill.spiller2.oppgi_navn())
 print (spill.gjennomfoer_spill())
-print (spill)
+print (spill)'''
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Kode for å generere GUI'et
+
+class GUITournament(Frame):
+    # Klassen GUITournament definerer en turnering mellom menneske
+    # og en Spiller
+    spiller = None
+    # Resultater holder resultatet av kampene - for plotting
+    resultater = None
+    # Denne labelen vil brukes for aa rapportere enkeltspill
+    resultat_label = None
+
+    def __init__(self, parent, motspiller):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        # Huske hvem vi spiller mot
+        self.spiller = motspiller
+        # Initiere listen av resultater
+        self.resultater = []
+        # Foreloepig ikke noe aa rapportere
+        self.resultat_label = StringVar()
+        self.resultat_label.set("Beskrivelse av siste spill kommer her")
+        self.style = Style()
+        self.fig = None
+
+    def setup_gui(self):
+        self.parent.title("Stein - Saks - Papir")
+        self.style.theme_use("default")
+        self.pack(fill=BOTH, expand=1)
+        # Label for rapportering
+        label = Label(self.parent, textvariable=self.resultat_label)
+        label.place(x=800, y=50)
+        # Buttons
+        # Disse fyrer av metoden self.arranger_enkeltspill som er
+        # definert i klassen. Denne metoden tar aksjonen til mennesket
+        # som startup, og gjennomfoerer spillet
+        # Samme type oppfoersel for de tre aksjons-knappene
+        rock_button = Button(self, text="Stein",
+                             command=lambda: self.arranger_enkeltspill(Aksjon("Stein")))
+        rock_button.place(x=800, y=400)
+        scissors_button = Button(self, text="Saks",
+                                 command=lambda: self.arranger_enkeltspill(Aksjon("Saks")))
+        scissors_button.place(x=900, y=400)
+        paper_button = Button(self, text="Papir",
+                              command=lambda: self.arranger_enkeltspill(Aksjon("Papir")))
+        paper_button.place(x=1000, y=400)
+        # quit_button avslutter GUI'et naar den trykkes
+        quit_button = Button(self, text="Quit", command=self.quit)
+        quit_button.place(x=1000, y=450)
+        # Embedde en graf i vinduet for aa rapportere fortloepende score
+        self.fig = FigureCanvasTkAgg(pylab.figure(), master=self)
+        self.fig.get_tk_widget().grid(column=0, row=0)
+        self.fig.show()
+
+        import matplotlib.pyplot as plt
+        import numpy
+        plt.figure(self.fig.figure.number)  # Handle til figuren
+        plt.ion()
+        plt.plot(range(1, len(self.resultater) + 1),
+                 100 * numpy.cumsum(self.resultater) /
+                 range(1, len(self.resultater) + 1), 'b-', lw=4)
+        plt.ylim([0, 100])
+        plt.xlim([1, max(1.1, len(self.resultater))])
+        plt.plot(plt.xlim(), [50, 50], 'k--', lw=2)
+        plt.grid(b=True, which='both', color='0.65', linestyle='-')
+        self.fig.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Styrer spiller gjennom Tkinter/GUI.
+root = Tk()
+# Definer et vindu med gitte dimensjoner
+root.geometry("1100x500+300+300")
+# Lag instans, og kjoer oppsett av GUI (knapper etc)
+GUITournament(root, Historiker(2)).setup_gui()
+# Vis vindu, og utfoer tilhoerende kommandoer
+root.mainloop()
 
 
 
